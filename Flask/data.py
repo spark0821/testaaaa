2f04af108b7f02eb4c8df26322e7c77529190984
thomas2017 <huang1996413@gmail.com>
2018-10-11 20:19:07 +0800

    fuck this

---
+++ Flask/data.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 21:00:26 2018
+
+@author: gaga
+"""
+
+def Articles():
+    articles = [
+        {
+                'id':1,
+                'title':'Article One',
+                'body':'Because the article system is so complex and often idiosyncratic, it is especially difficult for non-native English speakers to master. This handout explains three basic rules that are the foundation of the article system and two basic questions that will help you choose the correct article in your writing. It provides examples of articles being used in context, and it ends with a section on special considerations for nouns in academic writing.',
+                'author':'lebron james',
+                'create_data':'09-10-2018'
+                           
+        },
+        {
+                'id':2,
+                'title':'Article Two',
+                'body':'Because the article system is so complex and often idiosyncratic, it is especially difficult for non-native English speakers to master. This handout explains three basic rules that are the foundation of the article system and two basic questions that will help you choose the correct article in your writing. It provides examples of articles being used in context, and it ends with a section on special considerations for nouns in academic writing.',
+                'author':'kobe james',
+                'create_data':'09-10-2018'
+                           
+        },
+        {
+                'id':3,
+                'title':'Article Three',
+                'body':'Because the article system is so complex and often idiosyncratic, it is especially difficult for non-native English speakers to master. This handout explains three basic rules that are the foundation of the article system and two basic questions that will help you choose the correct article in your writing. It provides examples of articles being used in context, and it ends with a section on special considerations for nouns in academic writing.',
+                'author':'lebron bryant',
+                'create_data':'09-10-2018'
+                           
+        } 
+    ]
+    
+    return articles
